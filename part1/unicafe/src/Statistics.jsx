3b513e9145bbde8530817f9e7a7ff9d4cdcd998c
Oswaldo <rodriosw13@gmail.com>
2025-12-28 00:35:20 -0400

    Part 1: Exercise 1.10 step 5

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,27 +1,25 @@
+import StatisticsLine from './StatisticsLine.jsx'
+
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad
   const average = total === 0 ? 0 : (good - bad) / total
   const positivePercentage = total === 0 ? 0 : (good / total) * 100
   return (
     <>
+      <h2>Statistics</h2>
       {total === 0 ? (
-        <div>
-          <h2>Statistics</h2>
-          <p>No feedback given</p>
-        </div>
+        <p>No feedback given</p>
       ) : (
         <>
-          <h2>Statistics</h2>
-          <p>good: {good}</p>
-          <p>neutral: {neutral}</p>
-          <p>bad: {bad}</p>
-          <p>all {total} </p>
-          <p>average {average} </p>
-          <p>positive {positivePercentage}%</p>
+          <StatisticsLine text='good' value={good} />
+          <StatisticsLine text='neutral' value={neutral} />
+          <StatisticsLine text='bad' value={bad} />
+          <StatisticsLine text='total' value={total} />
+          <StatisticsLine text='average' value={average} />
+          <StatisticsLine text='positive' value={`${positivePercentage}%`} />
         </>
       )}
     </>
   )
 }
-
 export default Statistics
