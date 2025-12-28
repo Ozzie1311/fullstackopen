964452051f9732566b6415518ec9be66ff9b5260
Oswaldo <rodriosw13@gmail.com>
2025-12-28 00:22:14 -0400

    Part 1: Exercise 1.9 step 4

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -4,13 +4,22 @@ const Statistics = ({ good, neutral, bad }) => {
   const positivePercentage = total === 0 ? 0 : (good / total) * 100
   return (
     <>
-      <h2>Statistics</h2>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all {total} </p>
-      <p>average {average} </p>
-      <p>positive {positivePercentage}%</p>
+      {total === 0 ? (
+        <div>
+          <h2>Statistics</h2>
+          <p>No feedback given</p>
+        </div>
+      ) : (
+        <>
+          <h2>Statistics</h2>
+          <p>good: {good}</p>
+          <p>neutral: {neutral}</p>
+          <p>bad: {bad}</p>
+          <p>all {total} </p>
+          <p>average {average} </p>
+          <p>positive {positivePercentage}%</p>
+        </>
+      )}
     </>
   )
 }
