a98e1761e67fb961d8b1a0419f4dc980ace4ca37
Oswaldo <rodriosw13@gmail.com>
2026-01-13 00:12:12 -0400

    Part 3: Exercise 3.14

--- part3/index.js
+++ part3/index.js
@@ -16,33 +16,23 @@ app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
 
-// let phonebook = [
-//   {
-//     id: 1,
-//     name: 'Arto Hellas',
-//     number: '040-123456',
-//   },
-//   {
-//     id: 2,
-//     name: 'Ada Lovelace',
-//     number: '39-44-5323523',
-//   },
-//   {
-//     id: 3,
-//     name: 'Dan Abramov',
-//     number: '12-43-234345',
-//   },
-//   {
-//     id: 4,
-//     name: 'Mary Poppendieck',
-//     number: '39-23-6423122',
-//   },
-// ]
+app.post('/api/persons', (req, res) => {
+  const body = req.body
 
-// const generateRandomNumber = () => {
-//   return Math.floor(Math.random() * 1000000)
-// }
+  if (body.name === undefined || body.number === undefined) {
+    return res.status(404).json({ error: 'content missing' })
+  }
 
+  const newPerson = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  newPerson.save().then((savedPerson) => {
+    console.log(savedPerson, 'added succesfully')
+    res.json(savedPerson)
+  })
+})
 // app.post('/api/persons', (request, response) => {
 //   const body = request.body
 
