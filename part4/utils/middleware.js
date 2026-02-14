a40ae798356e425901aa1c4aa4f5bc45f24a9aa8
Oswaldo <rodriosw13@gmail.com>
2026-02-13 21:11:27 -0400

    Part 4: Exercise 4.18-4.20

--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -7,6 +7,16 @@ const requestLogger = (request, response, next) => {
     next()
 }
 
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        request.token = authorization.replace('Bearer ', '')
+    } else {
+        request.token = null
+    }
+    next()
+}
+
 const unknownEndpoint = (request, response) => {
     return response.status(404).send({ error: 'unknown Endpoint' })
 }
@@ -36,4 +46,9 @@ const errorHandler = (error, request, response, next) => {
     next(error)
 }
 
-module.exports = { requestLogger, unknownEndpoint, errorHandler }
+module.exports = {
+    requestLogger,
+    unknownEndpoint,
+    errorHandler,
+    tokenExtractor,
+}
