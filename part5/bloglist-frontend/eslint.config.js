970458fb8115d62496787c632569ec5a03863331
Oswaldo <rodriosw13@gmail.com>
2026-02-18 21:19:44 -0400

    Part5: Exercise 5.12

--- part5/bloglist-frontend/eslint.config.js
+++ part5/bloglist-frontend/eslint.config.js
@@ -4,30 +4,71 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module'
-      }
+    { ignores: ['dist'] },
+    {
+        files: ['**/*.{js,jsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                ecmaFeatures: { jsx: true },
+                sourceType: 'module',
+            },
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            ...reactHooks.configs.recommended.rules,
+            'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            // indent: ['error', 4],
+            'linebreak-style': ['error', 'unix'],
+            quotes: ['error', 'single'],
+            semi: ['error', 'never'],
+            eqeqeq: 'error',
+            'no-trailing-spaces': 'error',
+            'object-curly-spacing': ['error', 'always'],
+            'arrow-spacing': ['error', { before: true, after: true }],
+            'no-console': 'off',
+        },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true }
-      ]
-    }
-  }
 ]
+// import js from '@eslint/js'
+// import globals from 'globals'
+// import stylistic from '@stylistic/eslint-plugin'
+// import { defineConfig, globalIgnores } from 'eslint/config'
+
+// export default defineConfig([
+//     { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+//     {
+//         files: ['**/*.{js,mjs,cjs}'],
+//         languageOptions: { globals: globals.node },
+//         plugins: { js, stylistic },
+//         extends: ['js/recommended'],
+//         rules: {
+//             eqeqeq: 'error',
+//             'no-trailing-spaces': ['error'],
+//             'object-curly-spacing': ['error', 'always'],
+//             'no-console': 0,
+//             'space-before-function-paren': [
+//                 'error',
+//                 {
+//                     anonymous: 'always',
+//                     named: 'always',
+//                     asyncArrow: 'always',
+//                 },
+//             ],
+//             'stylistic/indent': ['error', 4],
+//             'no-unused-vars': ['error'],
+//             'no-undef': ['error'],
+//         },
+//     },
+//     globalIgnores(['./dist/']),
+// ])
--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -8,115 +8,119 @@ import LoginForm from './components/LoginForm'
 import BlogForm from './components/BlogForm'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [user, setUser] = useState(null)
+    const [blogs, setBlogs] = useState([])
+    const [errorMessage, setErrorMessage] = useState(null)
+    const [user, setUser] = useState(null)
 
-  useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs))
-  }, [])
+    useEffect(() => {
+        blogService.getAll().then((blogs) => setBlogs(blogs))
+    }, [])
 
-  useEffect(() => {
-    const loggedUser = window.localStorage.getItem('loggedBlogUser')
-    if (loggedUser) {
-      const userFromStorage = JSON.parse(loggedUser)
-      setUser(userFromStorage)
-      blogService.setToken(userFromStorage.token)
+    useEffect(() => {
+        const loggedUser = window.localStorage.getItem('loggedBlogUser')
+        if (loggedUser) {
+            const userFromStorage = JSON.parse(loggedUser)
+            setUser(userFromStorage)
+            blogService.setToken(userFromStorage.token)
+        }
+    }, [])
+
+    const handleFormLogin = async (userObject) => {
+        try {
+            const user = await loginService.login(userObject)
+            window.localStorage.setItem('loggedBlogUser', JSON.stringify(user))
+            blogService.setToken(user.token)
+            setUser(user)
+        } catch {
+            notificationMessage('wrong credentials')
+        }
     }
-  }, [])
 
-  const handleFormLogin = async (userObject) => {
-    try {
-      const user = await loginService.login(userObject)
-      window.localStorage.setItem('loggedBlogUser', JSON.stringify(user))
-      blogService.setToken(user.token)
-      setUser(user)
-    } catch {
-      notificationMessage('wrong credentials')
+    const notificationMessage = (message) => {
+        setErrorMessage(message)
+        setTimeout(() => {
+            setErrorMessage(null)
+        }, 5000)
     }
-  }
 
-  const notificationMessage = (message) => {
-    setErrorMessage(message)
-    setTimeout(() => {
-      setErrorMessage(null)
-    }, 5000)
-  }
-
-  const handleBlogForm = (blogObject) => {
-    blogService.create(blogObject).then((returnedBlog) => {
-      setBlogs(blogs.concat(returnedBlog))
-      notificationMessage(
-        `a new blog ${returnedBlog.title} by ${returnedBlog.author} added`,
-      )
-    })
-  }
-
-  const handleLogout = () => {
-    window.localStorage.removeItem('loggedBlogUser')
-    window.localStorage.clear()
-    setUser(null)
-  }
-
-  const handleLikeButton = async (id) => {
-    const blog = blogs.find((blog) => blog.id === id)
-    const updatedBlog = {
-      ...blog,
-      likes: blog.likes + 1,
+    const handleBlogForm = (blogObject) => {
+        blogService.create(blogObject).then((returnedBlog) => {
+            setBlogs(blogs.concat(returnedBlog))
+            notificationMessage(
+                `a new blog ${returnedBlog.title} by ${returnedBlog.author} added`
+            )
+        })
     }
-    const returnedBlog = await blogService.update(id, updatedBlog)
-    setBlogs(
-      blogs.map((blog) =>
-        blog.id !== id ? blog : { ...returnedBlog, user: blog.user },
-      ),
-    )
-  }
 
-  const deleteBlog = async (id) => {
-    const findBlog = await blogs.find((blog) => blog.id === id)
-    if (window.confirm(`Remove blog ${findBlog.title} by ${findBlog.author}`)) {
-      try {
-        await blogService.deleteBlog(id)
-        setBlogs(blogs.filter((blog) => blog.id !== id))
-      } catch {
-        setErrorMessage('Cannot eliminate the blog')
-      }
+    const handleLogout = () => {
+        window.localStorage.removeItem('loggedBlogUser')
+        window.localStorage.clear()
+        setUser(null)
     }
-  }
 
-  const sortedBlogsByLikes = [...blogs].sort((a, b) => b.likes - a.likes)
+    const handleLikeButton = async (id) => {
+        const blog = blogs.find((blog) => blog.id === id)
+        const updatedBlog = {
+            ...blog,
+            likes: blog.likes + 1,
+        }
+        const returnedBlog = await blogService.update(id, updatedBlog)
+        setBlogs(
+            blogs.map((blog) =>
+                blog.id !== id ? blog : { ...returnedBlog, user: blog.user }
+            )
+        )
+    }
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {errorMessage && <Notification message={errorMessage} />}
-      {user === null ? (
-        <Togglable buttonLabel='Login'>
-          <LoginForm userLogin={handleFormLogin} />
-        </Togglable>
-      ) : (
+    const deleteBlog = async (id) => {
+        const findBlog = await blogs.find((blog) => blog.id === id)
+        if (
+            window.confirm(
+                `Remove blog ${findBlog.title} by ${findBlog.author}`
+            )
+        ) {
+            try {
+                await blogService.deleteBlog(id)
+                setBlogs(blogs.filter((blog) => blog.id !== id))
+            } catch {
+                setErrorMessage('Cannot eliminate the blog')
+            }
+        }
+    }
+
+    const sortedBlogsByLikes = [...blogs].sort((a, b) => b.likes - a.likes)
+
+    return (
         <div>
-          <p>{user.name} logged in</p>
-          <button onClick={handleLogout}>logout</button>
-          {user !== null && (
-            <Togglable buttonLabel='create new blog'>
-              <BlogForm createBlog={handleBlogForm} />
-            </Togglable>
-          )}
+            <h2>blogs</h2>
+            {errorMessage && <Notification message={errorMessage} />}
+            {user === null ? (
+                <Togglable buttonLabel="Login">
+                    <LoginForm userLogin={handleFormLogin} />
+                </Togglable>
+            ) : (
+                <div>
+                    <p>{user.name} logged in</p>
+                    <button onClick={handleLogout}>logout</button>
+                    {user !== null && (
+                        <Togglable buttonLabel="create new blog">
+                            <BlogForm createBlog={handleBlogForm} />
+                        </Togglable>
+                    )}
+                </div>
+            )}
+            {user !== null
+                ? sortedBlogsByLikes.map((blog) => (
+                      <Blog
+                          handleLikes={handleLikeButton}
+                          handleDelete={deleteBlog}
+                          key={blog.id}
+                          blog={blog}
+                      />
+                  ))
+                : null}
         </div>
-      )}
-      {user !== null
-        ? sortedBlogsByLikes.map((blog) => (
-            <Blog
-              handleLikes={handleLikeButton}
-              handleDelete={deleteBlog}
-              key={blog.id}
-              blog={blog}
-            />
-          ))
-        : null}
-    </div>
-  )
+    )
 }
 
 export default App
