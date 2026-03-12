9d19e90285563738df3fae86488cf083ad394269
Oswaldo <rodriosw13@gmail.com>
2026-03-11 20:05:19 -0400

    Part 6: Exercise 6.10

--- part6/redux-anecdotes/src/components/Notification.jsx
+++ part6/redux-anecdotes/src/components/Notification.jsx
@@ -1,12 +1,15 @@
+import { useSelector } from 'react-redux'
+
 const Notification = () => {
+  const notification = useSelector((state) => state.anecdotes)
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1,
-    marginBottom: 10
+    marginBottom: 10,
   }
 
-  return <div style={style}>render here notification...</div>
+  return <div style={style}>{notification}</div>
 }
 
 export default Notification
--- part6/redux-anecdotes/src/tests/anecdotesTest.test.js
+++ part6/redux-anecdotes/src/tests/anecdotesTest.test.js
@@ -1,3 +1,19 @@
 import deepFreeze from 'deep-freeze'
 import { describe, test, expect } from 'vitest'
 import anecdoteReducer from '../reducers/anecdoteReducer'
+
+describe('anecdoteReducer', () => {
+  test('returns new state with action anecdote/addAnecdote', () => {
+    const state = []
+    const action = {
+      type: 'anecdote/addAnecdote',
+      payload: 'the app state is in redux store',
+    }
+
+    deepFreeze(state)
+    const newState = anecdoteReducer(state, action)
+
+    expect(newState).toHaveLength(1)
+    expect(newState.map((a) => a.content)).toContainEqual(action.payload)
+  })
+})
