448b57cf8a606ffe7cdfff33d7b50e6e4b1d0547
Oswaldo <rodriosw13@gmail.com>
2026-03-11 21:36:52 -0400

    Part 6: Exercise 6.13

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,5 +1,6 @@
 import { useDispatch } from 'react-redux'
 import { addAnecdote } from '../reducers/anecdoteReducer'
+import { showNotification } from '../reducers/notificationReducer'
 
 export const AnecdoteForm = () => {
   const dispatch = useDispatch()
@@ -9,6 +10,7 @@ export const AnecdoteForm = () => {
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     dispatch(addAnecdote(content))
+    dispatch(showNotification(`Anecdote created with content: ${content}`, 3))
   }
 
   return (
